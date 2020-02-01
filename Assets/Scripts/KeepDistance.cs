a78ff6a2907f13f1640365c2cf1d1d4e9ed566ee
Sashiri <ali.reanc@gmail.com>
2020-02-01 11:49:55 +0100

    Platforms and background update

---
+++ Assets/Scripts/KeepDistance.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepDistance : MonoBehaviour
+{
+    public GameObject target;
+    Vector3 offset;
+    void Start()
+    {
+        offset = target.transform.position - gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        gameObject.transform.position = target.transform.position - offset;
+    }
+}
