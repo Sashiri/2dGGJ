085f2e0dcb18cdd5ff4031aa6b69b6e860485cfb
Sashiri <ali.reanc@gmail.com>
2020-02-01 16:54:48 +0100

    Level Platforms and Level 1 scene

---
+++ Assets/Scripts/EnableDisableInteraction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisableInteraction : InteractableObject
+{
+    // Start is called before the first frame update
+    public SceneSettings sceneSettings;
+    public List<GameObject> toEnable;
+    public List<GameObject> toDisable;
+    public int neededItems = 3;
+
+    public override void InteractWithObject()
+    {
+        base.InteractWithObject();
+        if (Inventory.Instance.itemCount.Value >= neededItems)
+        {
+            PickUpItem();
+        }
+        else
+        {
+            sceneSettings.levelHint.SetActive(true);
+        }
+    }
+
+    private void PickUpItem()
+    {
+        sceneSettings.playerAnimator.Play("PickUp");
+        foreach (var enable in toEnable)
+        {
+            enable.SetActive(true);
+        }
+        foreach (var disable in toDisable)
+        {
+            disable.SetActive(false);
+        }
+    }
+
+}
--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PickUp : InteractableObject
 {
-    // Start is called before the first frame update
     public Animator PlayerAnimator;
     public override void InteractWithObject()
     {
@@ -15,11 +14,11 @@ public class PickUp : InteractableObject
     private void PickUpItem()
     {
         Debug.Log("PickUpItem " + transform.name);
-        
+
         Inventory.Instance.AddItem(this);
         PlayerAnimator.Play("PickUp");
         //Inventory.Instance.AddItem(transform.name);
         Destroy(gameObject);
     }
-    
+
 }
