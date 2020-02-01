7428ef97364760d41198396dce5f0ad997174aa4
Sashiri <ali.reanc@gmail.com>
2020-02-01 10:24:42 +0100

    Add observers

---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Observer<T>
+{
+    public delegate void VoidCall(T value);
+    private T value;
+    public T Value
+    {
+        get
+        {
+            return value;
+        }
+        set
+        {
+            this.value = value;
+            Inform();
+        }
+    }
+    public event VoidCall subscribers;
+    public Observer(ref T obj)
+    {
+        value = obj;
+    }
+    public Observer(T obj)
+    {
+        value = obj;
+    }
+    public Observer() { }
+    public void Inform()
+    {
+        subscribers?.Invoke(value);
+    }
+}
