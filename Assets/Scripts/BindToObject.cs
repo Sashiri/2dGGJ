24e98bc056e66efbf187e1f0656ffe5751f62aaf
Sashiri <ali.reanc@gmail.com>
2020-02-01 02:50:54 +0100

    Movable tiles

---
+++ Assets/Scripts/BindToObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BindToObject : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        other.gameObject.transform.SetParent(this.gameObject.transform);
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        other.gameObject.transform.SetParent(null);
+    }
+}
