1ab9b44c36c8788f466fae2495e8e3273cc62981
Sashiri <ali.reanc@gmail.com>
2020-02-02 10:01:03 +0100

    Level1 Update

---
+++ Assets/EnableOnEnter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableOnEnter : MonoBehaviour
+{
+    public GameObject objectToEnable;
+    public GameObject objectToDisable;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (objectToEnable) objectToEnable.SetActive(true);
+        if (objectToDisable) objectToDisable?.SetActive(false);
+    }
+}
