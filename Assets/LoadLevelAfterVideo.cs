7bf61d75d4312f01e3dfc2b2f329e40dd6b97d54
Sashiri <ali.reanc@gmail.com>
2020-02-02 15:18:17 +0100

    Cinematics

---
+++ Assets/LoadLevelAfterVideo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelAfterVideo : MonoBehaviour
+{
+    public UnityEngine.Video.VideoPlayer cinematic;
+    public string levelName;
+    private void Start()
+    {
+        cinematic.loopPointReached += (_) => SceneManager.LoadScene(levelName);
+        cinematic.Play();
+    }
+}
